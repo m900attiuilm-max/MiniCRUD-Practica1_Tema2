1c5af0a22e8ba29af82fd43bea6e681325cd3c11
m900attiuilm-max <m900attiuilm@gmail.com>
2026-09-17 08:41:02 -0300

    Update PersonaVO.java
    Agregar atributo categoria, lo agregue al constructor y su getter y setter en PersonaVO

--- src/main/java/modelo/vo/PersonaVO.java
+++ src/main/java/modelo/vo/PersonaVO.java
@@ -5,15 +5,18 @@ public class PersonaVO {
     private String nombre;
     private double sueldo;
     private String email;
+     private String categoria;
 
     public PersonaVO() {
+
     }
 
-    public PersonaVO(int cod, String nombre, double sueldo, String email) {
+    public PersonaVO(int cod, String nombre, double sueldo, String email,  String categoria) {
         this.cod = cod;
         this.nombre = nombre;
         this.sueldo = sueldo;
         this.email = email;
+        this.categoria = categoria;
     }
 
     public int getCodigo() {
@@ -40,7 +43,7 @@ public class PersonaVO {
         this.sueldo = sueldo;
     }
 
-    public String getEmail() {
+ public String getEmail() {
         return email;
     }
 
@@ -48,9 +51,19 @@ public class PersonaVO {
         this.email = email;
     }
 
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+
+
     @Override
     public String toString() {
-        return "PersonaVO{cod=" + cod + ", nombre=" + nombre
-                + ", sueldo=" + sueldo + ", email=" + email + '}';
+        return "PersonaVO{" + "cod=" + cod + ", nombre=" + nombre + ", sueldo=" + sueldo    + '}';
     }
+
 }
